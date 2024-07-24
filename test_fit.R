e925de1745b9ff9679717705ce4781c41909b085
Annie Booth <annie_booth@ncsu.edu>
2024-07-24 13:09:14 -0400

    fits for toy data

---
+++ test_fit.R
@@ -0,0 +1,88 @@
+
+# Goal: test some GP and DGP fits to the toy data
+# How does the DGP do?  Can it dimension reduct?
+
+library(deepgp)
+set.seed(1)
+
+dat <- read.csv("random500.csv")
+x <- as.matrix(dat[, -ncol(dat)])
+y <- dat[, ncol(dat)]
+
+# Train/test split
+train <- sample(1:nrow(dat), 200, replace = FALSE)
+xtrain <- x[train, ]
+ytrain <- y[train]
+xtest <- x[-train, ]
+ytest <- y[-train]
+
+# Isotropic GP
+gp <- fit_one_layer(xtrain, ytrain, nmcmc = 10000, sep = FALSE, true_g = 1e-6)
+plot(gp$ll, type = "l")
+gp <- trim(gp, 5000, 5)
+gp <- predict(gp, xtest, lite = TRUE)
+gp_rmse <- round(rmse(ytest, gp$mean), 3)
+gp_crps <- round(crps(ytest, gp$mean, gp$s2), 3)
+
+# Separable GP
+gpsep <- fit_one_layer(xtrain, ytrain, nmcmc = 10000, sep = TRUE, true_g = 1e-6)
+plot(gpsep$ll, type = "l") # did not get many acceptances, sticky chain
+gpsep <- trim(gpsep, 5000, 5)
+gpsep <- predict(gpsep, xtest, lite = TRUE)
+gpsep_rmse <- round(rmse(ytest, gpsep$mean), 3)
+gpsep_crps <- round(crps(ytest, gpsep$mean, gpsep$s2), 3)
+
+# Two-layer DGP with D = 8
+dgp8 <- fit_two_layer(xtrain, ytrain, D = 8, nmcmc = 10000, true_g = 1e-6)
+plot(dgp8$ll, type = "l")
+dgp8 <- trim(dgp8, 5000, 5)
+dgp8 <- predict(dgp8, xtest, lite = TRUE)
+dgp8_rmse <- round(rmse(ytest, dgp8$mean), 3)
+dgp8_crps <- round(crps(ytest, dgp8$mean, dgp8$s2), 3)
+
+# Two-layer DGP with D = 4
+dgp4 <- fit_two_layer(xtrain, ytrain, D = 4, nmcmc = 10000, true_g = 1e-6)
+plot(dgp4$ll, type = "l")
+dgp4 <- trim(dgp4, 5000, 5)
+dgp4 <- predict(dgp4, xtest, lite = TRUE)
+dgp4_rmse <- round(rmse(ytest, dgp4$mean), 3)
+dgp4_crps <- round(crps(ytest, dgp4$mean, dgp4$s2), 3)
+
+# Two-layer DGP with D = 2
+dgp2 <- fit_two_layer(xtrain, ytrain, D = 2, nmcmc = 10000, true_g = 1e-6)
+plot(dgp2$ll, type = "l")
+dgp2 <- trim(dgp2, 5000, 5)
+dgp2 <- predict(dgp2, xtest, lite = TRUE)
+dgp2_rmse <- round(rmse(ytest, dgp2$mean), 3)
+dgp2_crps <- round(crps(ytest, dgp2$mean, dgp2$s2), 3)
+
+pred <- data.frame(y = ytest,
+                   gpmean = gp$mean,
+                   gps2 = gp$s2,
+                   gpsepmean = gpsep$mean,
+                   gpseps2 = gpsep$s2,
+                   dgp8mean = dgp8$mean,
+                   dgp8s2 = dgp8$s2,
+                   dgp4mean = dgp4$mean,
+                   dgp4s2 = dgp4$s2,
+                   dgp2mean = dgp2$mean,
+                   dgp2s2 = dgp2$s2)
+write.csv(pred, "pred.csv", row.names = FALSE)
+
+results <- data.frame(model = c("gp", "gpsep", "dgp8", "dgp4", "dgp2"),
+                      rmse = c(gp_rmse, gpsep_rmse, dgp8_rmse, dgp4_rmse, dgp2_rmse),
+                      crps = c(gp_crps, gpsep_crps, dgp8_crps, dgp4_crps, dgp2_crps))
+write.csv(results, "results.csv", row.names = FALSE)
+
+par(mfrow = c(2, 3))
+plot(ytest, gp$mean, main = "Isotropic GP", xlab = "Actual", ylab = "Predicted")
+abline(0, 1, col = 2)
+plot(ytest, gpsep$mean, main = "Separable GP", xlab = "Actual", ylab = "Predicted")
+abline(0, 1, col = 2)
+plot(0, pch = NA, xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
+plot(ytest, dgp8$mean, main = "DGP 8 nodes", xlab = "Actual", ylab = "Predicted")
+abline(0, 1, col = 2)
+plot(ytest, dgp4$mean, main = "DGP 4 nodes", xlab = "Actual", ylab = "Predicted")
+abline(0, 1, col = 2)
+plot(ytest, dgp2$mean, main = "DGP 2 nodes", xlab = "Actual", ylab = "Predicted")
+abline(0, 1, col = 2)
