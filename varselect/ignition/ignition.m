b58ed70cb246281b507bcb4dcc7b8f511853a34e
annie <annie_booth@ncsu.edu>
2025-11-11 10:38:55 -0500

    zhang method took forever, just running bk

--- varselect/ignition/ignition.m
+++ varselect/ignition/ignition.m
@@ -3,11 +3,11 @@ addpath('~/Competitors/variable_selection')
 addpath('~/MATLAB_pkgs/ooDACE-1.4/ooDACE')
 
 d = 13;
-n = 400;
-np = 500;
+n = 200;
+np = 1000;
 reps = 50;
 
-for seed = 2:reps
+for seed = 1:reps
   rng(seed)
   x = lhsdesign(n, d);
   y = ignitionfunc(x);
@@ -15,32 +15,32 @@ for seed = 2:reps
   yp = ignitionfunc(xp);
 
   % Zhang method 
-  results = Indicator_based_bayesian_vs_gp(x, y, ...
-      'lambda', 1, ... % default 1
-      'tau', 1, ... % default 1
-      'sigma', 1, ... % default 1
-      'q', 0.5, ... % default 0.5
-      'iterations', 500, ... % default 2000
-      'burnin', 250, ... % default 1000
-      'scaling', 3, ...
-      'verbose', 1);
-  zhang_results = readtable('results/zhang_probs.csv');
-  zhang_results{seed, 2:(d+1)} = num2cell(results.active_prob);
-  writetable(zhang_results, 'results/zhang_probs.csv');
+  %results = Indicator_based_bayesian_vs_gp(x, y, ...
+  %    'lambda', 1, ... % default 1
+  %    'tau', 1, ... % default 1
+  %    'sigma', 1, ... % default 1
+  %    'q', 0.5, ... % default 0.5
+  %    'iterations', 500, ... % default 2000
+  %    'burnin', 250, ... % default 1000
+  %    'scaling', 3, ...
+  %    'verbose', 1);
+  %zhang_results = readtable('results/zhang_probs.csv');
+  %zhang_results{seed, 2:(d+1)} = num2cell(results.active_prob);
+  %writetable(zhang_results, 'results/zhang_probs.csv');
 
   % GP with only the selected variables
-  xtrim = x(:, results.active_vars);
-  xptrim = xp(:, results.active_vars);
-  fit = oodacefit(xtrim, y);
-  [mu s2] = fit.predict(xptrim);
-  rmse = sqrt(mean((yp - mu).^2));
-  s = sqrt(s2);
-  z = (yp - mu)./s;
-  crps = mean(s.*(-1/sqrt(pi) + 2.*normpdf(z) + z.*(2.*normcdf(z)-1)));
-  r = readtable('results/pred_zhang.csv');
-  r.RMSE{seed} = rmse;
-  r.CRPS{seed} = crps;
-  writetable(r, 'results/pred_zhang.csv');
+  %xtrim = x(:, results.active_vars);
+  %xptrim = xp(:, results.active_vars);
+  %fit = oodacefit(xtrim, y);
+  %[mu s2] = fit.predict(xptrim);
+  %rmse = sqrt(mean((yp - mu).^2));
+  %s = sqrt(s2);
+  %z = (yp - mu)./s;
+  %crps = mean(s.*(-1/sqrt(pi) + 2.*normpdf(z) + z.*(2.*normcdf(z)-1)));
+  %r = readtable('results/pred_zhang.csv');
+  %r.RMSE{seed} = rmse;
+  %r.CRPS{seed} = crps;
+  %writetable(r, 'results/pred_zhang.csv');
 
   % Blind Kriging method (code from demo.m in ooDACE toolbox)
   opts.type = 'BlindKriging';
